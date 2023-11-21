644b6e57f0037580aff8a714b778ca37b4d2b489
Tom .M <107270908+Tom7053@users.noreply.github.com>
2023-11-21 12:24:56 +0100

    Update

--- arduino.ino
+++ arduino.ino
@@ -17,21 +17,30 @@
  * $RCSfile: main.c,v $
  * $Date: 2023/11/21 10:43:51 $  \n
  ******************************************************************************/
+//#include <avr/io.h>
 #define FOSC 16000000 // Clock Speed
 #define BAUD 9600
 #define MYUBRR FOSC / 16 / BAUD - 1
+int flag=0;
+volatile char donnee;
 
 /*! \brief Fonction Main 
  */
 int main(void) {
   USART_Init(MYUBRR);
+  sei();
+  //USART_Transmit(USART_Receive()+1);
   while(1){
-  USART_Transmit(USART_Receive());
-  USART_Transmit('T');
-  USART_Transmit('o');
+    if(flag)
+    {
+      USART_Transmit(UDR0+1);
+      flag=0;
+    }
+  _delay_ms(1);
+  /*else{USART_Transmit('T');*/
+  /*USART_Transmit('o');
   USART_Transmit('m');
-  USART_Transmit('\n');
-  _delay_ms(1000);
+  USART_Transmit('\n');*/
   }
 }
 
@@ -42,7 +51,7 @@ void USART_Init(unsigned int ubrr) {
   UBRR0H = (unsigned char)(ubrr >> 8);
   UBRR0L = (unsigned char)ubrr;
   //Enable receiver and transmitter * /
-  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
   /* Set frame format: 8data, 2stop bit */
   UCSR0C = (1 << USBS0) | (3 << UCSZ00);
 }
@@ -66,4 +75,10 @@ while (!(UCSR0A & (1<<RXC0)))
 ;
 /* Get and return received data from buffer */
 return UDR0;
+}
+
+/*! \brief https://www.nongnu.org/avr-libc/user-manual/group__avr__interrupts.html
+ */
+ISR(USART_RX_vect){
+  flag=1;
 }
