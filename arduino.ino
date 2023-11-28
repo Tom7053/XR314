15f90cdb7dacd7c30c6badad4805b681be65f087
Tom .M <107270908+Tom7053@users.noreply.github.com>
2023-11-28 10:52:47 +0100

    Add files via upload

--- arduino.ino
+++ arduino.ino
@@ -18,11 +18,11 @@
  * $Date: 2023/11/21 10:43:51 $  \n
  ******************************************************************************/
 //#include <avr/io.h>
-#define FOSC 16000000 // Clock Speed
+#define FOSC 16000000  // Clock Speed
 #define BAUD 9600
 #define MYUBRR FOSC / 16 / BAUD - 1
-int flag=0;
-volatile char donnee;
+int flag = 0;
+
 
 /*! \brief Fonction Main 
  */
@@ -30,17 +30,10 @@ int main(void) {
   USART_Init(MYUBRR);
   sei();
   //USART_Transmit(USART_Receive()+1);
-  while(1){
-    if(flag)
-    {
-      USART_Transmit(UDR0+1);
-      flag=0;
-    }
-  _delay_ms(1);
-  /*else{USART_Transmit('T');*/
-  /*USART_Transmit('o');
-  USART_Transmit('m');
-  USART_Transmit('\n');*/
+  while (1) {
+    _delay_ms(1);
+    USART_putsln("IUT");
+
   }
 }
 
@@ -58,27 +51,39 @@ void USART_Init(unsigned int ubrr) {
 
 /*! \brief Fonction Transmission de la communication
  */
-void USART_Transmit(unsigned char data)
-{
-/* Wait for empty transmit buffer */
-while (!(UCSR0A & (1<<UDRE0)));
-/* Put data into buffer, sends the data */
-UDR0 = data;
+void USART_Transmit(unsigned char data) {
+  /* Wait for empty transmit buffer */
+  while (!(UCSR0A & (1 << UDRE0)))
+    ;
+  /* Put data into buffer, sends the data */
+  UDR0 = data;
 }
 
 /*! \brief Fonction Reception de la communication
  */
-unsigned char USART_Receive(void)
+/*unsigned char USART_Receive(void)
 {
 /* Wait for data to be received */
-while (!(UCSR0A & (1<<RXC0)))
-;
+//while (!(UCSR0A & (1<<RXC0)))
+//;
 /* Get and return received data from buffer */
-return UDR0;
-}
+//return UDR0;
+//}
 
 /*! \brief https://www.nongnu.org/avr-libc/user-manual/group__avr__interrupts.html
  */
-ISR(USART_RX_vect){
-  flag=1;
+ISR(USART_RX_vect) {
+  flag = 1;
+}
+
+void USART_puts(unsigned char *str) {
+  do {
+    USART_Transmit(*str);
+  } while (*++str != 0);
+}
+
+void USART_putsln(unsigned char *str) {
+    USART_puts(str);
+    USART_Transmit(10);
+    USART_Transmit(13);
 }
