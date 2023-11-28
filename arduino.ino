74a3b413e2b8356c40a377ff76c3cf7ba2943873
Tom .M <107270908+Tom7053@users.noreply.github.com>
2023-11-28 11:15:37 +0100

    Add files via upload

--- arduino.ino
+++ arduino.ino
@@ -18,11 +18,15 @@
  * $Date: 2023/11/21 10:43:51 $  \n
  ******************************************************************************/
 //#include <avr/io.h>
-#define FOSC 16000000 // Clock Speed
+#define FOSC 16000000  // Clock Speed
 #define BAUD 9600
 #define MYUBRR FOSC / 16 / BAUD - 1
-int flag=0;
-volatile char donnee;
+
+#define FRAMING_ERROR (1 << FE0)
+#define PARITY_ERROR (1 << UPE0)
+#define DATA_OVERRUN (1 << DOR0)
+int flag = 0;
+
 
 /*! \brief Fonction Main 
  */
@@ -30,17 +34,18 @@ int main(void) {
   USART_Init(MYUBRR);
   sei();
   //USART_Transmit(USART_Receive()+1);
-  while(1){
-    if(flag)
-    {
-      USART_Transmit(UDR0+1);
-      flag=0;
+  while (1) {
+    if (flag == 1) {
+      char status = UCSR0A;
+      char byteReceive = UDR0;
+      if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN)) == 0) {
+        USART_Transmit(byteReceive);
+      } else {
+        USART_putsln("!error");
+      }
+      flag = 0;
     }
-  _delay_ms(1);
-  /*else{USART_Transmit('T');*/
-  /*USART_Transmit('o');
-  USART_Transmit('m');
-  USART_Transmit('\n');*/
+    _delay_ms(1);
   }
 }
 
@@ -58,27 +63,41 @@ void USART_Init(unsigned int ubrr) {
 
 /*! \brief Fonction Transmission de la communication
  */
-void USART_Transmit(unsigned char data)
-{
-/* Wait for empty transmit buffer */
-while (!(UCSR0A & (1<<UDRE0)));
-/* Put data into buffer, sends the data */
-UDR0 = data;
+void USART_Transmit(unsigned char data) {
+  /* Wait for empty transmit buffer */
+  while (!(UCSR0A & (1 << UDRE0)))
+    ;
+  /* Put data into buffer, sends the data */
+  UDR0 = data;
 }
 
 /*! \brief Fonction Reception de la communication
  */
-unsigned char USART_Receive(void)
-{
-/* Wait for data to be received */
-while (!(UCSR0A & (1<<RXC0)))
-;
-/* Get and return received data from buffer */
-return UDR0;
+unsigned char USART_Receive(void) {
+  /*Wait for data to be received */
+  while (!(UCSR0A & (1 << RXC0)))
+    ;
+  /* Get and return received data from buffer */
+  return UDR0;
 }
+
 
 /*! \brief https://www.nongnu.org/avr-libc/user-manual/group__avr__interrupts.html
  */
-ISR(USART_RX_vect){
-  flag=1;
+ISR(USART_RX_vect) {
+  flag = 1;
+}
+/*! \brief Fonction Transmission d'une chaine de caractères
+ */
+void USART_puts(unsigned char *str) {
+  do {
+    USART_Transmit(*str);
+  } while (*++str != 0);
+}
+/*! \brief Fonction Transmission d'une chaine de caractères avec un retour à la ligne
+ */
+void USART_putsln(unsigned char *str) {
+  USART_puts(str);
+  USART_Transmit(10);
+  USART_Transmit(13);
 }
